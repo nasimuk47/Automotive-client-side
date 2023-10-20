759abfc1066f243d0b632f91d5b1aac7315c7cef
nasimuk47 <nasim.uk47@gmail.com>
2023-10-20 12:42:04 +0600

    add PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "./Auth/Authprovider";
 
-const PrivateRoute = () => {
-    return <div></div>;
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <span className="loading loading-spinner loading-lg"></span>;
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate to="/Login" />;
 };
 
 export default PrivateRoute;
